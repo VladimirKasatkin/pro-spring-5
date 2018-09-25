a4effa66fe8daf76ec46c21367e5f0f477c11cdf
Vladimir Kasatkin <kasatkin@saritasa.com>
2018-09-25 10:17:04 +0700

    Update test=data to use proper DB

--- chapter06/plain-jdbc/src/main/resources/db/test-data.sql
+++ chapter06/plain-jdbc/src/main/resources/db/test-data.sql
@@ -1,15 +1,15 @@
-insert into singer (first_name, last_name, birth_date) values ('John', 'Mayer', '1977-10-16');
-insert into singer (first_name, last_name, birth_date) values ('Eric', 'Clapton', '1945-03-30');
-insert into singer (first_name, last_name, birth_date) values ('John', 'Butler', '1975-04-01');
+insert into musicdb.singer (first_name, last_name, birth_date) values ('John', 'Mayer', '1977-10-16');
+insert into musicdb.singer (first_name, last_name, birth_date) values ('Eric', 'Clapton', '1945-03-30');
+insert into musicdb.singer (first_name, last_name, birth_date) values ('John', 'Butler', '1975-04-01');
 
-insert into album (singer_id, title, release_date) values (1, 'The Search For Everything', '2017-01-20');
-insert into album (singer_id, title, release_date) values (1, 'Battle Studies', '2009-11-17');
-insert into album (singer_id, title, release_date) values (2, ' From The Cradle ', '1994-09-13');
+insert into musicdb.album (singer_id, title, release_date) values (1, 'The Search For Everything', '2017-01-20');
+insert into musicdb.album (singer_id, title, release_date) values (1, 'Battle Studies', '2009-11-17');
+insert into musicdb.album (singer_id, title, release_date) values (2, ' From The Cradle ', '1994-09-13');
 
 /* view the inserted data */
-select * from singer;
-select * from album;
+select * from musicdb.singer;
+select * from musicdb.album;
 
 /* simple join sample */
-select * from album a, singer s
+select * from musicdb.album a, musicdb.singer s
 where singer_id = s.id and s.id=1;
