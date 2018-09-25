aaa098369662f0bfa9a9cda3d6ac25386a22c70d
Vladimir Kasatkin <kasatkin@saritasa.com>
2018-09-25 10:13:44 +0700

    Fix cases in ddl code

--- chapter06/plain-jdbc/src/main/resources/db/ddl.sql
+++ chapter06/plain-jdbc/src/main/resources/db/ddl.sql
@@ -1,10 +1,10 @@
-CREATE USER 'prospring5'@'localhost' IDENTIFIED WITH mysql_native_password BY 'prospring5';
-CREATE USER 'prospring5'@'172.21.0.1' IDENTIFIED WITH mysql_native_password BY 'prospring5';
+create user 'prospring5'@'localhost' identified with mysql_native_password by 'prospring5';
+create user 'prospring5'@'172.21.0.1' identified with mysql_native_password by 'prospring5';
 
-CREATE SCHEMA MUSICDB;
-GRANT ALL PRIVILEGES ON MUSICDB . * TO 'prospring5'@'localhost';
-GRANT ALL PRIVILEGES ON MUSICDB . * TO 'prospring5'@'172.21.0.1';
-FLUSH PRIVILEGES;
+create schema musicdb;
+grant all privileges on musicdb . * to 'prospring5'@'localhost';
+grant all privileges on musicdb . * to 'prospring5'@'172.21.0.1';
+flush privileges;
 
-/*in case of java.sql.SQLException: The server timezone value 'UTC' is unrecognized or represents more than one timezone. */
-SET GLOBAL time_zone = '+3:00';
+/*in case of java.sql.sqlexception: the server timezone value 'utc' is unrecognized or represents more than one timezone. */
+set global time_zone = '+3:00';
